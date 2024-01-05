ebaf109c5d822e6d28d99eb22c5d0d1477c95771
tjennat <theo.jennat98@gmail.com>
2024-01-05 02:51:18 -0800

    function nop

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - Implements the nop opcode
+ * @stack: The stack
+ * @line_number: Line number in the file
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
