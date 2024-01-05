6c7a8af63d191113f4a14c1f1f1f33486266dfc6
tjennat <theo.jennat98@gmail.com>
2024-01-05 03:09:13 -0800

    adding in stack

---
+++ add.c
@@ -0,0 +1,19 @@
+nclude "monty.h"
+
+/**
+ * add - Implements the add opcode
+ * @stack: The stack
+ * @line_number: Line number in the file
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
